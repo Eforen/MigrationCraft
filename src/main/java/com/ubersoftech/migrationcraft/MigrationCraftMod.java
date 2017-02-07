2a27ff55538ac54b7a13222a05f8cb089a0a9f46
Ariel Lothlorien <git@arielkauai.com>
2017-02-07 06:42:00 -0600

    refactored Example

--- src/main/java/com/ubersoftech/migrationcraft/MigrationCraftMod.java
+++ src/main/java/com/ubersoftech/migrationcraft/MigrationCraftMod.java
@@ -1,12 +1,12 @@
-package com.example.examplemod;
+package com.ubersoftech.migrationcraft;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = MigrationCraftMod.MODID, version = MigrationCraftMod.VERSION)
+public class MigrationCraftMod
 {
     public static final String MODID = "migrationcraft";
     public static final String VERSION = "0.1.0";
